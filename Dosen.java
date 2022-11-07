f1a898fcc1944fe8dcbc005b02b3acea344bd096
LutfiaAulia <100273637+LutfiaAulia@users.noreply.github.com>
2022-11-07 10:40:05 +0700

    Update Dosen.java

--- Dosen.java
+++ Dosen.java
@@ -3,11 +3,11 @@ class Dosen extends Elemen{
     private int jumlahHK;
 
     public Dosen (String nama, int jumlahHK){
-        super (nama);
+        super (nama); //Downcasting
         setJumlahHK (jumlahHK);
         this.jumlahHK = jumlahHK*8;
     }
-
+    //Polymorphisme
     public int getJamsibuk(){
         System.out.println(getNama()+"adalah seorang dosen dengan jam sibuk "+jumlahHK);
         return this.jumlahHK;
