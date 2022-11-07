a7a06f9769a7e8eacf11a638facba173bd1f6f13
LutfiaAulia <100273637+LutfiaAulia@users.noreply.github.com>
2022-11-07 10:41:42 +0700

    Update Mahasiswa.java

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -3,11 +3,11 @@ class Mahasiswa extends Elemen{
     private int SKS;
 
     public Mahasiswa(String nama, int SKS){
-        super(nama);
+        super(nama); //Downcasting
         setSKS(SKS);
         this.SKS = SKS*3;
     }
-
+    //Polymorphisme
     public int getJamsibuk (){
          System.out.println(getNama()+"adalah seorang mahasiswa dengan jam sibuk "+ SKS);
          return SKS;
